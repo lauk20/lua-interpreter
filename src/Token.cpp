f1c9526e72c3d177bf88392e563d14097def9808
lauk20 <72945813+lauk20@users.noreply.github.com>
2023-07-16 17:39:53 -0400

    Fix Token class definitions

--- src/Token.cpp
+++ src/Token.cpp
@@ -4,22 +4,12 @@
 
 #include "Token.hpp"
 
-class Token {
-    TokenType type;
-    std::string lexeme;
-    std::any literal;
-    int line;
-
-    public:
-        Token(TokenType type, std::string lexeme, std::any literal, int line) {
-            this->type = type;
-            this->lexeme = lexeme;
-            this->literal = literal;
-            this->line = line;
-        }
-
-        friend std::ostream& operator<<(std::ostream &os, const Token& token);
-};
+Token::Token(TokenType type, std::string lexeme, std::any literal, int line) {
+    this->type = type;
+    this->lexeme = lexeme;
+    this->literal = literal;
+    this->line = line;
+}
 
 std::ostream& operator<<(std::ostream& os, const Token& token) {
     return os << token.type << " " << token.lexeme << " " << token.line << std::endl;
