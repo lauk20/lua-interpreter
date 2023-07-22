29f481ff570afddfd0fddab2cf49ff46d7afbe99
lauk20 <72945813+lauk20@users.noreply.github.com>
2023-07-22 14:33:42 -0400

    remove generics from Expr header

--- src/Expr.hpp
+++ src/Expr.hpp
@@ -5,20 +5,14 @@
 
 #include "Token.hpp"
 
-template <class T>
 class Binary;
 
-template <class T>
 class Grouping;
 
-template <class T>
 class Literal;
 
-template <class T>
 class Unary;
 
-template <class T>
 class ExprVisitor;
 
-template <class T>
 class Expr;
