9350d8664d10c0ecf598da57cea0b16e0303f407
lauk20 <72945813+lauk20@users.noreply.github.com>
2023-07-22 13:41:26 -0400

    Add Expr header file

---
+++ src/Expr.hpp
@@ -0,0 +1,24 @@
+#include <any>
+#include <vector>
+#include <memory>
+#include <variant>
+
+#include "Token.hpp"
+
+template <class T>
+class Binary;
+
+template <class T>
+class Grouping;
+
+template <class T>
+class Literal;
+
+template <class T>
+class Unary;
+
+template <class T>
+class ExprVisitor;
+
+template <class T>
+class Expr;
